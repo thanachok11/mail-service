20569c7733fb387a7e754aa27c48469bc8a986b0
Thanachok Suwan <119285237+thanachok11@users.noreply.github.com>
2026-02-26 15:41:01 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MailModule } from "./mail/mail.module";
+import { HealthController } from "./mail/mail.controller";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MailModule,
   ],
+  controllers: [HealthController],
 })
-export class AppModule {}
+export class AppModule { }
